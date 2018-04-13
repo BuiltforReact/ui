6e776154a382e761b735d11958c404cbaf9e7ea1
Abishek R Srikaanth <abishek@technicaliti.me>
2018-04-13 12:16:19 -0400

    Removed keys from index.js and enabled scrollToTop on pagination

---
+++ next.config.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const webpack = require('webpack')
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env))
+    return config
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,56 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import {InstantSearch, Hits, SearchBox, Pagination, PoweredBy} from 'react-instantsearch/dom';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  PoweredBy,
+  ScrollTo
+} from 'react-instantsearch/dom';
 import Result from '../components/Result'
-function Index() {
-  return (<Layout>
-    <InstantSearch appId="O3D6FXC3ZH" apiKey="457b194f8671a2885cfc7c3c58910d4b" indexName="projects_index">
-      <main role="main">
-        <section className="jumbotron text-center">
-          <div className="container search-container">
-            <div className="form-group mx-sm-8">
-              <h1 className="title">
-                Search for a React Package</h1>
-              {/* <input type="text" className="form-control"/> */}
-              <SearchBox></SearchBox>
-              <div style={{
-                  marginTop: '10px',
-                  float: 'right'
-                }}>
-                <PoweredBy className="justify-content-right"></PoweredBy>
+import React from 'react'
+
+class Index extends React.Component {
+
+  state = {
+    appId: '',
+    apiKey: '',
+    indexName: ''
+  }
+  
+  render() {
+    return (<Layout>
+      <InstantSearch appId={`${process.env.APP_ID}`} apiKey={`${process.env.API_KEY}`} indexName={`${process.env.INDEX_NAME}`}>
+        <main role="main">
+          <section className="jumbotron text-center">
+            <div className="container search-container">
+              <div className="form-group mx-sm-8">
+                <h1 className="title">
+                  Search for a React Package</h1>
+                {/* <input type="text" className="form-control"/> */}
+                <SearchBox></SearchBox>
+                <div style={{
+                    marginTop: '10px',
+                    float: 'right'
+                  }}>
+                  <PoweredBy className="justify-content-right"></PoweredBy>
+                </div>
               </div>
             </div>
+          </section>
+          <div className="container result-container">
+            <ScrollTo>
+              <Hits hitComponent={Result}></Hits>
+            </ScrollTo>
           </div>
-        </section>
-        <div className="container result-container">
-          <Hits hitComponent={Result}></Hits>
-        </div>
-        <div className="container mt-5">
-          <Pagination showFirst={true} showLast={true} showPrevious={true} showNext={true} padding={2}></Pagination>
-        </div>
-      </main>
-    </InstantSearch>
-  </Layout>)
+          <div className="container mt-5">
+            <Pagination showFirst={true} showLast={true} showPrevious={true} showNext={true} padding={2}></Pagination>
+          </div>
+        </main>
+      </InstantSearch>
+    </Layout>)
+  }
 }
 
 export default Index
